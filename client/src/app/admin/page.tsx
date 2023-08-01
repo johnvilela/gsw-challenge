656d5d2ec8e3637787ce66e0068ca8721a339fa8
John Vilela <ba.john.vidal@gmail.com>
2023-08-01 12:14:48 -0300

    feat(client): add user managment

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -3,11 +3,15 @@
 import { Button } from "@/components/inputs/Button";
 import { Textfield } from "@/components/inputs/Textfield";
 import { useBanknotes } from "@/hooks/useBanknotes";
+import { useUsers } from "@/hooks/useUsers";
 import Link from "next/link";
+import { useRef } from "react";
 import { MdDelete } from "react-icons/md";
 
 export default function AdminPage() {
   const { notesAmount, saveNotesAmount, setNotesAmount } = useBanknotes();
+  const { createUser, users, deleteUser, status } = useUsers();
+  const userRef = useRef({ name: '', email: '' })
 
   return (
     <main className="grid min-h-screen place-content-center p-4 bg-teal-200">
@@ -18,13 +22,13 @@ export default function AdminPage() {
           </strong>
         </h1>
         <h2 className="text-teal-100 mb-4 uppercase">Amount of notes</h2>
-        <form className="grid gap-2 grid-cols-2 grid-rows-2">
+        <form className="grid gap-2 grid-cols-2 grid-rows-2" onSubmit={saveNotesAmount}>
           <Textfield type="number" label="R$ 100" value={notesAmount[100]} onChange={e => setNotesAmount(val => ({ ...val, '100': +e.target.value }))} />
           <Textfield type="number" label="R$ 50" value={notesAmount[50]} onChange={e => setNotesAmount(val => ({ ...val, '50': +e.target.value }))} />
           <Textfield type="number" label="R$ 20" value={notesAmount[20]} onChange={e => setNotesAmount(val => ({ ...val, '20': +e.target.value }))} />
           <Textfield type="number" label="R$ 10" value={notesAmount[10]} onChange={e => setNotesAmount(val => ({ ...val, '10': +e.target.value }))} />
           <div className="col-span-2 mt-2">
-            <Button onClick={saveNotesAmount}>
+            <Button type='submit'>
               Save update
             </Button>
           </div>
@@ -34,37 +38,36 @@ export default function AdminPage() {
 
         <h2 className="text-teal-100 mb-4 uppercase">Manage users</h2>
 
-        <form className="grid gap-2 grid-cols-2 grid-rows-2">
-          <Textfield label="Name" />
-          <Textfield label="Email" />
-          <div className="col-span-2 mt-2">
-            <Button>
-              Create user
-            </Button>
-          </div>
-        </form>
+        {status === 'loading' && (<p className="text-teal-100">Loading users...</p>)}
 
-        <ul>
-          <li className="py-4 border-b border-teal-300 flex justify-between items-center text-teal-100">
-            <p>User name</p>
-            <button type='button'>
-              <MdDelete size='1.5rem' />
-            </button>
-          </li>
-          <li className="py-4 border-b border-teal-300 flex justify-between items-center text-teal-100">
-            <p>User name</p>
-            <button type='button'>
-              <MdDelete size='1.5rem' />
-            </button>
-          </li>
-          <li className="py-4 border-b border-teal-300 flex justify-between items-center text-teal-100">
-            <p>User name</p>
-            <button type='button'>
-              <MdDelete size='1.5rem' />
-            </button>
-          </li>
-        </ul>
+        {status === 'success' && (<>
+          <form className="grid gap-2 grid-cols-2 grid-rows-2" onSubmit={() => createUser({ ...userRef.current })}>
+            <Textfield type='text' placeholder="Name" onChange={e => userRef.current.name = e.target.value} />
+            <Textfield type='email' placeholder="Email" onChange={e => userRef.current.email = e.target.value} />
+            <div className="col-span-2 mt-2">
+              <Button type="submit">
+                Create user
+              </Button>
+            </div>
+          </form>
 
+          <h3 className="text-teal-100 mt-8 uppercase">User list</h3>
+
+          <ul>
+            {users.map(user => (
+              <li key={user.id} className="py-4 border-b last:border-b-0 border-teal-300 flex justify-between items-center text-teal-100">
+                <p>{user.name}</p>
+                <button type='button' onClick={() => deleteUser(user.id)}>
+                  <MdDelete size='1.5rem' />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </>
+        )}
+
+        <div className="w-full border-b-2 border-teal-100 my-12" />
+              
         <Link href='/' className="flex justify-center items-center w-full p-4 font-bold text-teal-50">
           Go back
         </Link>
---
+++ client/src/hooks/useUsers.ts
@@ -0,0 +1,58 @@
+import { Api } from "@/services/api";
+import { User } from "@/types/User";
+import { useEffect, useState } from "react";
+
+export function useUsers() {
+  const [status, setStatus] = useState<'success' | 'loading' | 'error' | 'idle'>('idle')
+  const [users, setUsers] = useState<User[]>([])
+
+  async function createUser(data: Pick<User, 'name' | 'email'>) {
+    setStatus('loading')
+    try {
+      const res = (await Api.post('/user', data)).data
+      setStatus('success')
+      setUsers(val => [...val, res])
+    } catch (error) {
+      setStatus('error')
+      throw error
+    }
+  }
+
+  async function listUsers() {
+    setStatus('loading')
+    try {
+      const res = (await Api.get('/user')).data
+      setStatus('success')
+      setUsers(res)
+    } catch (error) {
+      setStatus('error')
+      throw error
+    }
+  }
+
+  async function deleteUser(id: string) {
+    setStatus('loading')
+    try {
+      await Api.delete(`/user/${id}`)
+
+      setUsers(val => val.filter((user) => user.id !== id))
+
+      setStatus('success')
+    } catch (error) {
+      setStatus('error')
+      throw error
+    }
+  }
+
+  useEffect(() => {
+    listUsers()
+  }, [])
+
+  return {
+    status,
+    users,
+    createUser,
+    listUsers,
+    deleteUser
+  }
+}
