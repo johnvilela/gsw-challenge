0230fb4e190b81edfe3bc9e1b8099da4956aa334
John Vilela <ba.john.vidal@gmail.com>
2023-08-01 02:46:04 -0300

    feat(backend): add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('GSW Challenge')
+    .addBearerAuth({
+      description: 'JWT Token',
+      type: 'apiKey',
+      name: 'authorization',
+      in: 'header',
+    })
+    .setVersion(process.env.npm_package_version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
